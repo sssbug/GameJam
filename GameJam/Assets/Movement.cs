a52a19b972ed1fc5fbadf1fcda5387d48246459f
Bedrican <bedrican.caliskan@gmail.com>
2020-12-18 23:19:26 +0300

    ucak eklendi hareket denenecek

---
+++ GameJam/Assets/Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float m_moveSpeed = 2;
+    private Quaternion rotationY;
+    protected Touch touch; 
+    [SerializeField]public float rotatespeed = 5f;
+    // Rigidbody rigidbody;
+    void Start()
+    {
+       // rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+   private void FixedUpdate()
+    {
+        //rigidbody.velocity = transform.forward * speed;
+        if (Input.touchCount > 0)
+        {
+            DirectUpdate();
+        }
+        transform.position += -transform.up * m_moveSpeed * Time.deltaTime;
+    }
+    private void DirectUpdate()
+    {
+        touch = Input.GetTouch(0);
+        if (touch.phase == TouchPhase.Moved)
+        {
+            rotationY = Quaternion.Euler(0f, touch.deltaPosition.x * rotatespeed * Time.deltaTime, 0f);
+
+            transform.rotation = rotationY * transform.rotation;
+        }
+
+    }
+   
+}
