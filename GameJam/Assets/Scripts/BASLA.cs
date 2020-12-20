c46cf254eec628f2f45461c778d25d01d6f10244
sbug <kml.ahm.99@gmail.com>
2020-12-20 13:29:39 +0300

    TAP TO START

---
+++ GameJam/Assets/Scripts/BASLA.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UnityStandardAssets.Utility
+{
+    public class BASLA : MonoBehaviour
+    {
+        [SerializeField] GameObject planePrefab;
+        [SerializeField] GameObject waypointPrefab;
+
+        GameObject plane;
+        List<int> usedIndexs = new List<int>();
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            CreateBot();
+        }
+
+        private void CreateBot()
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                int randomIndex = Random.Range(0, waypointPrefab.transform.childCount);
+
+                if (usedIndexs.Contains(randomIndex))
+                {
+                    i--;
+                }
+                else
+                {
+                    usedIndexs.Add(randomIndex);
+                    plane = Instantiate(planePrefab);
+                    plane.GetComponent<WaypointProgressTracker>().circuit = waypointPrefab.transform.GetChild(randomIndex).GetComponent<WaypointCircuit>();
+                }
+            }
+        }
+        public void Basla()
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
